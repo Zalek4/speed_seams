ea507f9e3134f4ec294c8d34e6e06c921b655852
Alex Hallenbeck <alexhallenbeck1@gmail.com>
2022-03-12 19:06:41 -0600

    Added links to Texel Density Checker and UVPM3 in preferences

--- speed_seams/properties/addon.py
+++ speed_seams/properties/addon.py
@@ -13,3 +13,11 @@ class SpeedSeamsProps(bpy.types.AddonPreferences):
         box = layout.box()
         col = box.column(align=True)
         row = col.row(align=True)
+        scale = 1.2
+
+        row.label(text="Texel Density Checker (3.3 or higher) and UVPackmaster 3 are recommended for maximum functionality", icon='INFO')
+        
+        row = col.row(align=True)
+        row.scale_y = scale
+        row.operator("wm.url_open", text="Texel Density Checker").url = "https://mrven.gumroad.com/l/CEIOR"
+        row.operator("wm.url_open", text="UVPackmaster 3").url = "https://blendermarket.com/products/uvpackmaster"
